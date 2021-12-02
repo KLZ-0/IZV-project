9b9d5d290a2a8de71ec8b9b9b8937f952c497d24
KLZ-0 <adrian@kalazi.com>
2021-12-02 22:37:41 +0100

    79

--- analysis.py
+++ analysis.py
@@ -6,20 +6,27 @@ import seaborn as sns
 import numpy as np
 import os
 
-# muzete pridat libovolnou zakladni knihovnu ci knihovnu predstavenou na prednaskach
+# muzete pridat libovolnou zakladni knihovnu
+# ci knihovnu predstavenou na prednaskach
 # dalsi knihovny pak na dotaz
 
 """ Ukol 1:
-načíst soubor nehod, který byl vytvořen z vašich dat. Neznámé integerové hodnoty byly mapovány na -1.
+načíst soubor nehod, který byl vytvořen z vašich dat.
+Neznámé integerové hodnoty byly mapovány na -1.
 
 Úkoly:
-- vytvořte sloupec date, který bude ve formátu data (berte v potaz pouze datum, tj sloupec p2a)
-- vhodné sloupce zmenšete pomocí kategorických datových typů. Měli byste se dostat po 0.5 GB. Neměňte však na kategorický typ region (špatně by se vám pracovalo s figure-level funkcemi)
-- implementujte funkci, která vypíše kompletní (hlubkou) velikost všech sloupců v DataFrame v paměti:
+- vytvořte sloupec date, který bude ve formátu data
+(berte v potaz pouze datum, tj sloupec p2a)
+- vhodné sloupce zmenšete pomocí kategorických datových typů.
+Měli byste se dostat po 0.5 GB.
+Neměňte však na kategorický typ region
+(špatně by se vám pracovalo s figure-level funkcemi)
+- implementujte funkci, která vypíše kompletní (hlubkou)
+velikost všech sloupců v DataFrame v paměti:
 orig_size=X MB
 new_size=X MB
 
-Poznámka: zobrazujte na 1 desetinné místo (.1f) a počítejte, že 1 MB = 1e6 B. 
+Poznámka: zobrazujte na 1 desetinné místo (.1f) a počítejte, že 1 MB = 1e6 B.
 """
 
 # Columns to be converted into categories - could "o" be float?
@@ -57,6 +64,7 @@ def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
 
 def plot_roadtype(df: pd.DataFrame, fig_location: str = None,
                   show_figure: bool = False):
+
     pass
 
 
@@ -79,6 +87,7 @@ if __name__ == "__main__":
 
     # tento soubor si stahnete sami, při testování pro hodnocení bude existovat
     accidents_df = get_dataframe("accidents.pkl.gz")
-    plot_roadtype(accidents_df, fig_location="01_roadtype.png", show_figure=True)
+    plot_roadtype(accidents_df, fig_location="01_roadtype.png",
+                  show_figure=True)
     plot_animals(accidents_df, "02_animals.png", True)
     plot_conditions(accidents_df, "03_conditions.png", True)
