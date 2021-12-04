c7b5b5f1210ab0b4c1861310769477342035ca0b
KLZ-0 <adrian@kalazi.com>
2021-12-04 16:11:47 +0100

    Cleanup + Remove comments + Optimize imports

--- analysis.py
+++ analysis.py
@@ -2,37 +2,11 @@
 # coding=utf-8
 from pathlib import Path
 
-import matplotlib.colors
-from matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sns
-import numpy as np
-import os
+from matplotlib import pyplot as plt
 
-# muzete pridat libovolnou zakladni knihovnu
-# ci knihovnu predstavenou na prednaskach
-# dalsi knihovny pak na dotaz
-
-""" Ukol 1:
-načíst soubor nehod, který byl vytvořen z vašich dat.
-Neznámé integerové hodnoty byly mapovány na -1.
-
-Úkoly:
-- vytvořte sloupec date, který bude ve formátu data
-(berte v potaz pouze datum, tj sloupec p2a)
-- vhodné sloupce zmenšete pomocí kategorických datových typů.
-Měli byste se dostat po 0.5 GB.
-Neměňte však na kategorický typ region
-(špatně by se vám pracovalo s figure-level funkcemi)
-- implementujte funkci, která vypíše kompletní (hlubkou)
-velikost všech sloupců v DataFrame v paměti:
-orig_size=X MB
-new_size=X MB
-
-Poznámka: zobrazujte na 1 desetinné místo (.1f) a počítejte, že 1 MB = 1e6 B.
-"""
-
-# Columns to be converted into categories - could "o" be float?
+# Columns to be converted into categories
 _category_cols = ["k", "p", "q", "t", "l", "i", "h"]
 
 
@@ -68,8 +42,6 @@ def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
 
     return df
 
-
-# Ukol 2: počty nehod v jednotlivých regionech podle druhu silnic
 
 def plot_roadtype(df: pd.DataFrame, fig_location: str = None,
                   show_figure: bool = False):
@@ -119,7 +91,6 @@ def plot_roadtype(df: pd.DataFrame, fig_location: str = None,
         plt.show()
 
 
-# Ukol3: zavinění zvěří
 def plot_animals(df: pd.DataFrame, fig_location: str = None,
                  show_figure: bool = False):
     # static things
@@ -171,7 +142,6 @@ def plot_animals(df: pd.DataFrame, fig_location: str = None,
         plt.show()
 
 
-# Ukol 4: Povětrnostní podmínky
 def plot_conditions(df: pd.DataFrame, fig_location: str = None,
                     show_figure: bool = False):
     # static things
@@ -239,11 +209,6 @@ def plot_conditions(df: pd.DataFrame, fig_location: str = None,
 
 
 if __name__ == "__main__":
-    # zde je ukazka pouziti, tuto cast muzete modifikovat podle libosti
-    # skript nebude pri testovani pousten primo, ale budou volany konkreni ¨
-    # funkce.
-
-    # tento soubor si stahnete sami, při testování pro hodnocení bude existovat
     accidents_df = get_dataframe("accidents.pkl.gz")
     plot_roadtype(accidents_df, fig_location="01_roadtype.png",
                   show_figure=True)
